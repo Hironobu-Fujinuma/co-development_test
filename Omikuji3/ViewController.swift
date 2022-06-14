464ecca6aaab3093c23062364206e47f3c887a09
Fujinuma <f1910575@gl.cc.uec.ac.jp>
2022-06-14 10:57:41 +0900

    Initial Commit

---
+++ Omikuji3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Omikuji3
+//
+//  Created by KashiLab on 2022/06/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func pullOmikuji(_ sender: Any) {
+        let items = ["大吉", "中吉"]
+        let r = Int(arc4random()) % items.count
+        resultBox.text = items[r]
+    }
+    @IBOutlet weak var resultBox: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
