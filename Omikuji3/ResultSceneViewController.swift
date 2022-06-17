7f3914f36324e246302b84830d0edfb44589510f
maeda <m1910603@edu.cc.uec.ac.jp>
2022-06-17 14:31:03 +0900

    ResultSceneViewController.swift を追加

---
+++ Omikuji3/ResultSceneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultSceneViewController.swift
+//  Eitan
+//
+//  Created by member on 2022/06/17.
+//
+
+import UIKit
+
+class ResultSceneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
